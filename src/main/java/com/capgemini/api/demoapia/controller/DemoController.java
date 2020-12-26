09c551ce02cf026d83e6f449e43f85a1373fe4be
SumitBhawsar <sumib.8.6@gmail.com>
2020-12-26 18:45:19 +0000

    adding client

---
+++ src/main/java/com/capgemini/api/demoapia/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.capgemini.api.demoapia.controller;
+
+import com.capgemini.api.demoapia.controller.dto.GetDetailsResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@RestController
+public class DemoController {
+
+    @Value("${apia.name}")
+    private String name;
+
+    @Value("${apia.version}")
+    private String version;
+
+    @Value("${apia.secret}")
+    private String secret;
+
+    @GetMapping(value = "details",produces = APPLICATION_JSON_VALUE)
+    public GetDetailsResponse getDetails(){
+        return new GetDetailsResponse(name, version, secret);
+    }
+}
---
+++ src/test/java/com/capgemini/api/demoapia/DemoApiAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.api.demoapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
