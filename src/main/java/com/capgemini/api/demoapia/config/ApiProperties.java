c0377eb76bf9381f00faec4df2ed28e97cfb9816
SumitBhawsar <sumib.8.6@gmail.com>
2020-12-26 18:54:16 +0000

    added refreshscope

---
+++ src/main/java/com/capgemini/api/demoapia/config/ApiProperties.java
@@ -0,0 +1,38 @@
+package com.capgemini.api.demoapia.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "apia")
+public class ApiProperties {
+    private String name;
+
+    private String version;
+
+    private String secret;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
--- src/main/java/com/capgemini/api/demoapia/controller/DemoController.java
+++ src/main/java/com/capgemini/api/demoapia/controller/DemoController.java
@@ -1,7 +1,10 @@
 package com.capgemini.api.demoapia.controller;
 
+import com.capgemini.api.demoapia.config.ApiProperties;
 import com.capgemini.api.demoapia.controller.dto.GetDetailsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @RestController
+@RefreshScope
 public class DemoController {
 
     @Value("${apia.name}")
@@ -20,8 +24,16 @@ public class DemoController {
     @Value("${apia.secret}")
     private String secret;
 
-    @GetMapping(value = "details",produces = APPLICATION_JSON_VALUE)
+    @Autowired
+    private ApiProperties apiProperties;
+
+    @GetMapping(value = "/details",produces = APPLICATION_JSON_VALUE)
     public GetDetailsResponse getDetails(){
         return new GetDetailsResponse(name, version, secret);
+    }
+
+    @GetMapping(value = "/details/confprop",produces = APPLICATION_JSON_VALUE)
+    public GetDetailsResponse getDetailsconfProp(){
+        return new GetDetailsResponse(apiProperties.getName(), apiProperties.getVersion(), apiProperties.getSecret());
     }
 }
